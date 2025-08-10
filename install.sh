8aa3028cf2ffd938183dcb2295472cb3c9ddbf09
nubnab <emkata654@gmail.com>
2025-08-10 21:01:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 efi_plaftorm=$(</sys/firmware/efi/fw_platform_size)
 
+pacman-key --init
+pacman -Sy jq ansible
+
 set -e
 
 echo "$efi_platform" | grep -qwE '32|64' || {
@@ -15,4 +18,5 @@ nc -dzw1 8.8.8.8 443 >/dev/null 2>&1 || {
 
 timedatectl set-ntp true
 
-fdisk -l
+#list all unpartitioned disks, needs improvement
+lsblk --json -o NAME,TYPE,FSTYPE,MOUNTPOINT | jq -r '.blockdevices[] | select(.type == "disk" and (.children == null or .children == [])) | .name'
