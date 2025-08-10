a5c84fd5c0d8cea4ce218c499d8fbfbc6f09fe7c
nubnab <emkata654@gmail.com>
2025-08-10 22:02:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,3 +27,21 @@ parted /dev/$DISK --script mkpart primary fat32 1MiB 513MiB
 parted /dev/$DISK --script set 1 esp on
 
 parted /dev/$DISK --script mkpart primary btrfs 513MiB 100%
+
+if [[ "$DISK" == nvme* ]]; then
+  EFI_PART="/dev/${DISK}p1"
+  ROOT_PART="/dev/${DISK}p2"
+else
+  EFI_PART="/dev/${DISK}1"
+  ROOT_PART="/dev/${DISK}2"
+fi
+
+mkfs.fat -F 32 /dev/$EFI_PART
+mkfs.btrfs /dev/$ROOT_PART
+
+mount /dev/$ROOT_PART /mnt
+
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+
+umount /mnt
