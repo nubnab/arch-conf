77f36ea7bf9b7e586d22ca3156fec2e8906569b9
nubnab <emkata654@gmail.com>
2025-08-15 13:11:52 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -4,5 +4,10 @@ timedatectl set-ntp true
 
 sudo sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/grub-btrfsd --syslog --timeshift-auto|' /etc/systemd/system/grub-btrfsd.service
 
-sudo systemctl daemon-reload
-sudo systemctl restart grub-btrfsd
+sudo systemctl enable grub-btrfsd
+
+sudo pacman -S --needed base-devel
+
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+yay -S timeshift-autosnap
