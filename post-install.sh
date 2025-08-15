509be2ea694670051edbdc5c496af21d8d964317
nubnab <emkata654@gmail.com>
2025-08-15 13:08:19 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -5,4 +5,4 @@ timedatectl set-ntp true
 sudo sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/grub-btrfsd --syslog --timeshift-auto|' /etc/systemd/system/grub-btrfsd.service
 
 sudo systemctl daemon-reload
-sudo systemctl restart grub-brtfsd
+sudo systemctl restart grub-btrfsd
