15e3f59fd761d982a415eb1cb0b2abb2a46f67b9
nubnab <emkata654@gmail.com>
2025-08-21 03:47:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -67,6 +67,9 @@ read -p "Enter your username: " USERNAME
 
 arch-chroot /mnt /bin/bash -c "
 ln -sf /usr/share/zoneinfo/Europe/Sofia /etc/localtime &&
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen &&
+locale-gen &&
+echo \"LANG=en_US.UTF-8\" >> /etc/locale.conf &&
 hwclock --systohc &&
 passwd && 
 useradd -m -G wheel -s /bin/bash ${USERNAME} &&
