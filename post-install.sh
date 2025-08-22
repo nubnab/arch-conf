29573c6ad9181a1571ead59e3308763f5e5c8f50
nubnab <emkata654@gmail.com>
2025-08-22 23:44:01 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -2,15 +2,9 @@
 
 timedatectl set-ntp true
 
-sudo sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/grub-btrfsd --syslog --timeshift-auto|' /etc/systemd/system/grub-btrfsd.service
-
-sudo systemctl enable grub-btrfsd
-
 sudo pacman -S --needed base-devel
 
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-
-yay -S timeshift-autosnap
 
 sudo sed -i '/^#\[multilib\]$/{n;s|^#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|}' /etc/pacman.conf
 sudo sed -i 's/^#\[multilib\]/\[multilib\]/' /etc/pacman.conf
