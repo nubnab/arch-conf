9649d653647885838bf434b28e97717790cd80a2
nubnab <emkata654@gmail.com>
2025-08-23 22:16:17 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -12,3 +12,7 @@ yay -Syu
 yay -S nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings
 
 sudo mkdir -p /etc/pacman.d/hooks/ && sudo mv nvidia.hook /etc/pacman.d/hooks/
+
+sudo pacman -S vim fastfetch alacritty thunar sddm waybar xdg-desktop-portal-hyprland hyprland firefox
+
+sudo systemctl enable sddm.service
