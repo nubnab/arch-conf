e19f5cee141f6c14bc0337dac0c8a02ba932e9f1
nubnab <emkata654@gmail.com>
2025-08-28 02:12:48 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -4,18 +4,18 @@ sudo pacman -Sy
 
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
-sed -i '/^#\[multilib\]$/{n;s|^#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|}' /etc/pacman.conf
-sed -i 's/^#\[multilib\]/\[multilib\]/' /etc/pacman.conf
+sudo sed -i '/^#\[multilib\]$/{n;s|^#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|}' /etc/pacman.conf
+sudo sed -i 's/^#\[multilib\]/\[multilib\]/' /etc/pacman.conf
 
 yay -Syu
 
 yay -S nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings
 
-sed -i 's|^MODULES=()|MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)|' /etc/mkinitcpio.conf
-sed -i '/^HOOKS=/ s|modconf kms keyboard|modconf keyboard|' /etc/mkinitcpio.conf
+sudo sed -i 's|^MODULES=()|MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)|' /etc/mkinitcpio.conf
+sudo sed -i '/^HOOKS=/ s|modconf kms keyboard|modconf keyboard|' /etc/mkinitcpio.conf
 
-mkdir -p /etc/pacman.d/hooks/ 
-mv nvidia.hook /etc/pacman.d/hooks/
+sudo mkdir -p /etc/pacman.d/hooks/ 
+sudo mv nvidia.hook /etc/pacman.d/hooks/
 
 #hyprlock
 #hypridle
@@ -27,7 +27,7 @@ mv nvidia.hook /etc/pacman.d/hooks/
 #gnome-themes-extra
 #gtk-engine-murrine
 
-pacman -Sy linux-headers vim fastfetch kitty thunar sddm waybar xdg-desktop-portal-hyprland hyprland \
+sudo pacman -Sy linux-headers vim fastfetch kitty thunar sddm waybar xdg-desktop-portal-hyprland hyprland \
            hyprpolkitagent firefox wget unzip rofi-wayland dunst swww qt5-wayland qt6-wayland cliphist \
            thunar-volman thunar-archive-plugin ark gvfs tumbler brightnessctl slurp \
            grim swappy ttf-font-awesome otf-font-awesome ttf-fira-sans ttf-fira-code \
@@ -42,6 +42,6 @@ yay -S waypaper hyprland-qtutils vesktop --noconfirm
 #proton
 #wine
 
-mkinitcpio -P
+sudo mkinitcpio -P
 
-systemctl enable sddm.service
+sudo systemctl enable sddm.service
