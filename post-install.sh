c938e85d7548eb4b199f75ea417394fb64579f1a
nubnab <emkata654@gmail.com>
2025-08-27 20:49:44 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -17,6 +17,18 @@ sed -i '/^HOOKS=/ s|modconf kms keyboard|modconf keyboard|' /etc/mkinitcpio.conf
 mkdir -p /etc/pacman.d/hooks/ 
 mv nvidia.hook /etc/pacman.d/hooks/
 
-pacman -Sy vim fastfetch kitty thunar sddm waybar xdg-desktop-portal-hyprland hyprland firefox
-
+pacman -Sy vim fastfetch kitty thunar sddm waybar xdg-desktop-portal-hyprland hyprland \
+           firefox wget unzip rofi-wayland dunst swww qt5-wayland qt6-wayland cliphist \
+           thunar-volman thunar-archive-plugin ark gvfs tumbler brightnessctl slurp \
+           grim swappy ttf-font-awesome otf-font-awesome ttf-fira-sans ttf-fira-code \
+           ttf-firacode-nerd noto-fonts-cjk nwg-look --noconfirm
+#hyprlock
+#hypridle
+#starship
+#picom -> screen tearing
+#network-manager-applet
+#blueman(?) bluez / bluez-utils
+#xclip unecessary
+#gnome-themes-extra
+#gtk-engine-murrine
 systemctl enable sddm.service
