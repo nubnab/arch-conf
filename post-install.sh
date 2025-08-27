34ccf79e610e61d2b30e6264a8c7e60c9a36cecd
nubnab <emkata654@gmail.com>
2025-08-27 17:23:04 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -6,15 +6,19 @@ sudo pacman -Sy
 
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
-sudo sed -i '/^#\[multilib\]$/{n;s|^#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|}' /etc/pacman.conf
-sudo sed -i 's/^#\[multilib\]/\[multilib\]/' /etc/pacman.conf
+sed -i '/^#\[multilib\]$/{n;s|^#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|}' /etc/pacman.conf
+sed -i 's/^#\[multilib\]/\[multilib\]/' /etc/pacman.conf
 
 yay -Syu
 
 yay -S nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings
 
-sudo mkdir -p /etc/pacman.d/hooks/ && sudo mv nvidia.hook /etc/pacman.d/hooks/
+sed -i 's|^MODULES=()|MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)|' /etc/mkinitcpio.conf
+sed -i '/^HOOKS=/ s|modconf kms keyboard|modconf keyboard|' /etc/mkinitcpio.conf
 
-sudo pacman -Sy vim fastfetch kitty thunar sddm waybar xdg-desktop-portal-hyprland hyprland firefox
+mkdir -p /etc/pacman.d/hooks/ 
+mv nvidia.hook /etc/pacman.d/hooks/
 
-sudo systemctl enable sddm.service
+pacman -Sy vim fastfetch kitty thunar sddm waybar xdg-desktop-portal-hyprland hyprland firefox
+
+systemctl enable sddm.service
