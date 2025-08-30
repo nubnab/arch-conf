163a225f3a9c50bb77f1fe3e2a9c16972079b6f2
nubnab <emkata654@gmail.com>
2025-08-31 01:10:06 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -20,7 +20,9 @@ sudo pacman -Sy linux-headers vim fastfetch kitty thunar sddm waybar xdg-desktop
                 ttf-fira-sans ttf-fira-code ttf-firacode-nerd noto-fonts-cjk --noconfirm
 
 yay -S hyprland-qtutils nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings \
-       waypaper vesktop qimgv jetbrains-toolbox wlogout qogir-gtk-theme qogir-icon-theme github-desktop --noconfirm
+       waypaper vesktop qimgv jetbrains-toolbox wlogout github-desktop-bin --noconfirm
+
+       #qogir theme
     
 sudo sed -i 's|^MODULES=()|MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)|' /etc/mkinitcpio.conf
 sudo sed -i '/^HOOKS=/ s|modconf kms keyboard|modconf keyboard|' /etc/mkinitcpio.conf
