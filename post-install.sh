d5447142f1395d5d7bfdecb37fe0dad1b34e56aa
nubnab <emkata654@gmail.com>
2025-08-30 22:35:44 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -17,8 +17,8 @@ sudo pacman -Sy linux-headers vim fastfetch kitty thunar sddm waybar xdg-desktop
                 thunar-volman thunar-archive-plugin ark unrar gvfs tumbler brightnessctl slurp nwg-look \
                 network-manager-applet grim swappy gnome-themes-extra gtk-engine-murrine steam starship \
                 mangohud lib32-mangohud bluez bluez-utils hyprlock hypridle gamemode vala lib32-gamemode \
-                yazi btop nvtop vulkan-tools mesa-utils ttf-font-awesome otf-font-awesome ttf-fira-sans \
-                ttf-fira-code ttf-firacode-nerd noto-fonts-cjk --noconfirm
+                yazi btop nvtop vulkan-tools mesa-utils qbittorrent ttf-font-awesome otf-font-awesome \
+                ttf-fira-sans ttf-fira-code ttf-firacode-nerd noto-fonts-cjk --noconfirm
 
 yay -S hyprland-qtutils nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings --noconfirm
 yay -S waypaper vesktop qimgv jetbrains-toolbox wlogout qogir-gtk-theme qogir-icon-theme github-desktop --noconfirm
