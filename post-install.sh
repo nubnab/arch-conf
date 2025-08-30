62bb7289c400da3466ee01968fc30d28c992c49b
nubnab <emkata654@gmail.com>
2025-08-31 00:51:40 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -4,7 +4,7 @@ sudo pacman -Sy
 
 base_dir=$(pwd)
 
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+cd "$base_dir" && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 sudo sed -i '/^#\[multilib\]$/{n;s|^#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|}' /etc/pacman.conf
 sudo sed -i 's/^#\[multilib\]/\[multilib\]/' /etc/pacman.conf
