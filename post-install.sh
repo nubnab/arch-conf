983c2d7c27f1596e104fbcdb50faad4a3872d234
nubnab <emkata654@gmail.com>
2025-09-03 15:08:45 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -13,14 +13,14 @@ yay -Sy
 
 sudo pacman -Sy linux-headers vim fastfetch kitty thunar sddm waybar xdg-desktop-portal-hyprland hyprland \
                 polkit-gnome firefox wget unzip rofi-wayland swaync swww qt5-wayland qt6-wayland cliphist \
-                thunar-volman thunar-archive-plugin ark unrar gvfs tumbler brightnessctl slurp nwg-look \
+                thunar-volman thunar-archive-plugin unrar gvfs tumbler brightnessctl slurp nwg-look \
                 network-manager-applet grim swappy gnome-themes-extra gtk-engine-murrine steam starship \
                 mangohud lib32-mangohud bluez bluez-utils hyprlock hypridle gamemode vala lib32-gamemode 
-                neovim yazi btop nvtop vulkan-tools mesa-utils qbittorrent gtk-engines neovide \
+                neovim yazi btop nvtop vulkan-tools mesa-utils rtorrent gtk-engines neovide \
                 ttf-font-awesome otf-font-awesome ttf-fira-sans ttf-fira-code ttf-firacode-nerd noto-fonts-cjk --noconfirm
 
 yay -S hyprland-qtutils nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings \
-       waypaper vesktop qimgv jetbrains-toolbox wlogout github-desktop-bin --noconfirm
+       waypaper vesktop qimgv jetbrains-toolbox wlogout --noconfirm
        
 sudo sed -i 's|^MODULES=()|MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)|' /etc/mkinitcpio.conf
 sudo sed -i '/^HOOKS=/ s|modconf kms keyboard|modconf keyboard|' /etc/mkinitcpio.conf
