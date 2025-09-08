8a9e3bdb69103776fa8daf46e131ed245a4964d0
nubnab <emkata654@gmail.com>
2025-09-08 10:49:27 +0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -11,16 +11,16 @@ sudo sed -i 's/^#\[multilib\]/\[multilib\]/' /etc/pacman.conf
 
 yay -Sy
 
-sudo pacman -S  linux-headers vim fastfetch kitty thunar sddm waybar xdg-desktop-portal-hyprland hyprland \
-                polkit-gnome firefox wget unzip rofi-wayland swaync swww qt5-wayland qt6-wayland cliphist \
+sudo pacman -S  linux-headers vim fastfetch kitty thunar sddm xdg-desktop-portal-hyprland hyprland \
+                polkit-gnome firefox wget unzip rofi-wayland swww qt5-wayland qt6-wayland cliphist \
                 thunar-volman thunar-archive-plugin unrar gvfs tumbler brightnessctl slurp nwg-look \
-                network-manager-applet grim swappy gnome-themes-extra gtk-engine-murrine steam starship \
-                mangohud lib32-mangohud bluez bluez-utils hyprlock hypridle gamemode vala lib32-gamemode 
-                neovim yazi btop nvtop vulkan-tools mesa-utils rtorrent gtk-engines neovide \
-                ttf-font-awesome otf-font-awesome ttf-fira-sans ttf-fira-code ttf-firacode-nerd noto-fonts-cjk --noconfirm
+                network-manager-applet grim swappy steam starship btop mangohud lib32-mangohud bluez \
+                bluez-utils gamemode vala lib32-gamemode libnewt neovim yazi uwsm nvtop vulkan-tools \
+                mesa-utils rtorrent gtk-engines neovide ttf-font-awesome otf-font-awesome ttf-fira-sans \
+                ttf-fira-code ttf-firacode-nerd noto-fonts-cjk --noconfirm
 
 yay -S hyprland-qtutils nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings \
-       waypaper vesktop qimgv jetbrains-toolbox wlogout --noconfirm
+       waypaper vesktop qimgv jetbrains-toolbox --noconfirm
        
 sudo sed -i 's|^MODULES=()|MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)|' /etc/mkinitcpio.conf
 sudo sed -i '/^HOOKS=/ s|modconf kms keyboard|modconf keyboard|' /etc/mkinitcpio.conf
@@ -42,6 +42,3 @@ sudo systemctl enable sddm.service
 cd "$base_dir"
 cp -rf "config/." ~/.config/
 cp -rf "local/." ~/.local/
-
-git clone https://github.com/vinceliuice/Qogir-theme && sh ./Qogir-theme/install.sh -c dark
-git clone https://github.com/vinceliuice/Qogir-icon-theme && sh ./Qogir-icon-theme/install.sh
